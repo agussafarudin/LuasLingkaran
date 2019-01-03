98d7eaf61005c930fb590e3c1bc04d5b44c58e3d
agussafarudin <shempal.sf@gmail.com>
2019-01-03 14:49:30 +0700

    v-01

---
+++ LuasLingkaran/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r,phi=3.14;
+    cout << "Softeare pembuat Luas Lingkaran \n \n";
+    cout << "Masukkan Jari-jari =";
+    cin >>r;
+    cout<<"\n \n";
+    cout << "Luas lingkaran     =";
+    cout <<(phi*r*r);
+}
